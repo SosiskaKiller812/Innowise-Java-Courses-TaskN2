9797145851c4c1467e456c98205d1fc57f19c7de
Vlad <vladislavofficial.812@gmail.com>
2026-01-29 15:33:06 +0300

    change the folder structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# Innowise Task N2(Information handling/ light)
+
+## Description
+
+
+## Technologies and tools
+* **Language:** Java 21+
+* **Assembly:** Maven 
+* **Testing:** JUnit 5
--- src/main/java/by/vladislav/task2/Main.java
+++ src/main/java/by/vladislav/task2/Main.java
@@ -1,7 +1,7 @@
 package by.vladislav.task2;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+
+  }
 }
